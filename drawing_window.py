acc3c06dedd2a9ab39425fec14f94c01c5e7a6c0
Daniel Tran <dhbtran@gmail.com>
2017-08-08 14:20:19 -0700

    Update GUI

--- drawing_window.py
+++ drawing_window.py
@@ -2,7 +2,7 @@ import tkinter
 import matplotlib.pyplot as plt
 from ctypes import windll
 from PIL import ImageGrab
-# from display_image import display_image_from_vector
+from display_image import display_image_from_vector
 from predict import make_prediction
 from process_image import process_image
 
@@ -14,8 +14,9 @@ class DrawingWindow:
         self.bottom = tkinter.Frame(self.main_window)
         self.bottom.pack(side=tkinter.BOTTOM)
         self.create_canvas()
-        self.create_submit_button()
+        self.create_predict_button()
         self.create_clear_button()
+        self.create_data_button()
         self.create_text_widget()
 
     def get_coords(self):
@@ -34,26 +35,36 @@ class DrawingWindow:
                          pady=10)
         self.canvas.bind("<B1-Motion>", self.paint)
 
-    def create_submit_button(self):
-        self.button = tkinter.Button(master=self.main_window,
-                                     text='Submit',
-                                     font=('Helvetica', 12))
-        self.button.pack(in_=self.bottom,
-                         side=tkinter.LEFT,
-                         padx=5,
-                         pady=8)
-        self.button.bind("<Button-1>", self.get_screenshot)
-        self.button.bind("<ButtonRelease-1>", self.show_letter)
+    def create_predict_button(self):
+        button = tkinter.Button(master=self.main_window,
+                                text='Predict',
+                                font=('Helvetica', 12))
+        button.pack(in_=self.bottom,
+                    side=tkinter.LEFT,
+                    padx=5,
+                    pady=8)
+        button.bind("<Button-1>", self.get_screenshot)
+        button.bind("<ButtonRelease-1>", self.show_letter)
+
+    def create_data_button(self):
+        button = tkinter.Button(master=self.main_window,
+                                text='Show Data',
+                                font=('Helvetica', 12))
+        button.pack(in_=self.bottom,
+                    side=tkinter.RIGHT,
+                    padx=5,
+                    pady=8)
+        button.bind("<ButtonRelease-1>", self.show_data)
 
     def create_clear_button(self):
-        self.button = tkinter.Button(master=self.main_window,
-                                     text='Clear',
-                                     font=('Helvetica', 12))
-        self.button.pack(in_=self.bottom,
-                         side=tkinter.RIGHT,
-                         padx=5,
-                         pady=8)
-        self.button.bind("<ButtonRelease-1>", self.clear_drawing)
+        button = tkinter.Button(master=self.main_window,
+                                text='Clear',
+                                font=('Helvetica', 12))
+        button.pack(in_=self.bottom,
+                    side=tkinter.RIGHT,
+                    padx=5,
+                    pady=8)
+        button.bind("<ButtonRelease-1>", self.clear_drawing)
 
     def create_text_widget(self):
         self.text = tkinter.Text(master=self.main_window,
@@ -62,12 +73,12 @@ class DrawingWindow:
                                  font=('Helvetica', 14))
         self.text.pack(side=tkinter.BOTTOM, padx=10, pady=5)
         self.text.tag_configure('tag-center', justify='center')
+        self.text.configure(state=tkinter.DISABLED)
 
     def paint(self, event):
-        black = "#000000"
         x1, y1 = (event.x - 10), (event.y - 10)
         x2, y2 = (event.x + 10), (event.y + 10)
-        self.canvas.create_oval(x1, y1, x2, y2, fill=black)
+        self.canvas.create_oval(x1, y1, x2, y2, fill='black')
 
     def get_screenshot(self, event):
         x = self.canvas.winfo_rootx()
@@ -76,28 +87,41 @@ class DrawingWindow:
         y1 = y + self.canvas.winfo_height() - 1
         ImageGrab.grab(bbox=(x, y, x1, y1)).save('letter.png')
 
-    def show_letter(self, event):
-        # try:
-        #     plt.close()
-        # except:
-        #     pass
+    def predict_letter(self):
         image = process_image('letter.png')
-        # display_image_from_vector(image.flatten())
         pred, conf = make_prediction(image)
+        return list([pred, conf])
+
+    def show_letter(self, event):
+        try:
+            plt.close()
+        except:
+            pass
+        pred, conf = self.predict_letter()
         self.text.configure(state=tkinter.NORMAL)
         self.text.delete('1.0', tkinter.END)
         self.text.insert("end",
-                         "Prediction: " + pred + \
+                         "Prediction: " + pred +
                          '\nConfidence: ' + conf + '%',
                          'tag-center')
         self.text.configure(state=tkinter.DISABLED)
+
+    def show_data(self, event):
+        try:
+            plt.close()
+        except:
+            pass
+        image = process_image('letter.png')
+        self.get_screenshot(event)
+        self.show_letter(event)
+        display_image_from_vector(image.flatten())
         plt.show(block=False)
 
     def clear_drawing(self, event):
-        # try:
-        #     plt.close()
-        # except:
-        #     pass
+        try:
+            plt.close()
+        except:
+            pass
         self.canvas.delete('all')
         self.text.configure(state=tkinter.NORMAL)
         self.text.delete('1.0', tkinter.END)
